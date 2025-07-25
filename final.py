14a7a9ecff8b344dc2d80c699d5de9d8fb2dda6a
Andressa <iyaass02@outlook.com>
2025-07-25 14:47:05 -0300

    Add files via upload

--- final.py
+++ final.py
@@ -7,7 +7,6 @@ from dash.dependencies import Input, Output
 
 # etapa 1, carregar e limpar os dados 
 # função para carregar dados com tratamento de exceção
-
 def load_data(file_path):
     try: 
         data= pd.read_csv(file_path, encoding='ISO-8859-1')
@@ -16,7 +15,7 @@ def load_data(file_path):
     except:
         print(f'Erro ao carregar os dados de {file_path}')
         return None 
-
+    
 # carregar os CSVs
 avengers_df = load_data('avengers.csv')
 drinks_df = load_data('drinks.csv')
@@ -34,8 +33,7 @@ def clean_data(df,numeric_columns):
         return df 
     except Exception as e:
         print(f'Erro ao limpar os dados: {e}')
-        return None 
-
+        return None    
 # limpar avengers
 avengers_df = clean_data(avengers_df, ['Appearances'])
 #limpar a tabela drinks
@@ -45,4 +43,70 @@ print('\nAvengers DataFrame após limpeza')
 print(avengers_df.head())
 print('\nDrinks DataFRame após limpeza')
 print(drinks_df.head())
-        
+
+# Etapa 3 vamos analisar os dados 
+def show_statistics(df, title):
+    print(f'\nEstatisticas descritivas de {title}')
+    print(df.describe())
+
+show_statistics(avengers_df, "Vingadores")
+show_statistics(drinks_df,"Consumo de Alcool")
+
+# Etapa 4, criação da visualização com plotly 
+def create_avengers_chart():
+    return px.bar(
+        avengers_df,
+        x = 'Name/Alias',
+        y = 'Appearances',
+        title = 'Numero de aparições dos Vingadores',
+        labels ={'Name/Alias':'Personagem', 'Appearances':'Numero de aparições'},
+        color = 'Gender'
+    )
+# função para criar o grafico de consumo de alcool 
+def create_drinks_chart():
+    return px.bar(
+        drinks_df,
+        x ='country',
+        y = 'total_litres_of_pure_alcohol',
+        title = 'Consumo de Alcool por país',
+        labels ={'country':'País','total_litres_of_pure_alcohol':'Litros de Alcool' },
+        color = 'total_litres_of_pure_alcohol'
+    )
+
+app = dash.Dash(__name__) 
+app.layout= html.Div([
+    html.H1('Analise de dados - Vingadores e Consumo de Alcool', style={'text-align':'center'}),
+    #dropdown para escolher a visualização
+    dcc.Dropdown(
+        id= 'dropdown-chart',
+        options=[
+            {'label':'Numero de Aparições dos Vingadores', 'value':'avengers'},
+            {'label':'Consumo de Alcool por País', 'value':'drinks'}
+        ],
+        value='avengers',
+        style={'width':'50%', 'margin':'auto'}
+    ),
+    dcc.Graph(id='gragh-output')
+])
+   
+# função de callback para atualizar o grafico baseado na escolha do dropdown
+@app.callback(
+    Output('gragh-output', 'figure'),
+    [Input('dropdown-chart', 'value')]
+)
+
+def update_graph(chart_type):
+    try:
+        if chart_type == 'avengers':
+            return create_avengers_chart()
+        elif chart_type == 'drinks':
+            return create_drinks_chart()
+    except Exception as e :
+        print(f"Erro ao gerar o grafico:{e}")
+        return{}
+
+if __name__ =='__main__':
+    try:
+        app.run(debug=True)
+    except Exception as e: 
+        print(f'Erro ao rodar o servidor Dash: {e}')
